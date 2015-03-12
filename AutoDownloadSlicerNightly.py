683f9dc3f30fd0e1c33859505afb8d7735a101f1
fbudin69500 <fbudin@unc.edu>
2015-03-12 15:42:40 -0400

    ENH: Addition of an option to give a log file instead of printing outputs on the standard output.

--- AutoDownloadSlicerNightly.py
+++ AutoDownloadSlicerNightly.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+from __future__ import print_function
 import urllib2
 from urllib2 import HTTPError
 import re
@@ -9,14 +10,22 @@ import shutil
 import argparse
 import sys
 
-
 parser = argparse.ArgumentParser(
 prog=sys.argv[0],
 description="This script downloads the nightly package of Slicer",
 usage=sys.argv[0]+" [<args>]")
 parser.add_argument("output_folder", help="Directory to install the nightly package")
+parser.add_argument("--log_file", help="File in which the log of the process will be stored")
 args = parser.parse_args()
 output_folder = args.output_folder
+if args.log_file:
+  try:
+    log = open(args.log_file, 'w')
+  except Exception as e:
+    print(e)
+    exit(1)
+else:
+  log = sys.stdout
 # Hard coded parameters
 output_file_name = "/tmp/Slicer-nightly.tar.gz"
 erase_older_nightly_slicer = True
@@ -31,7 +40,7 @@ try:
     response = opener.open(download_page_url)
 except HTTPError as e:
     content = e.read()
-    print content
+    print(content,file=log)
     exit(1)
 finally:
     html = response.read()
@@ -40,9 +49,9 @@ try:
     found = re.search('<a href="(.+?)" class="btn btn-warning">'+regexForLinux, html).group(1)
     # Creation of the "absolute URL"
     download_url = "http://download.slicer.org"+found
-    print "URL to download Slicer nightly: "+download_url
+    print("URL to download Slicer nightly:",download_url,file=log)
 except AttributeError:
-    print "Did not find download URL"
+    print("Did not find download URL",file=log)
     exit(1)
 
 # Download Slicer
@@ -50,7 +59,7 @@ u = urllib2.urlopen(download_url)
 f = open(output_file_name, 'wb')
 meta = u.info()
 file_size = int(meta.getheaders("Content-Length")[0])
-print "Downloading in: %s (%s MBytes)" % (output_file_name, file_size/1024/1024)
+print ("Downloading in: ", output_file_name, "(",file_size/1024/1024,"MB)",sep="",file=log)
 
 file_size_dl = 0
 block_sz = 8192
@@ -65,16 +74,27 @@ while True:
     # char(8) ->goes one character to the left without erase current character
     # This line allows to print the download progress allows at the same location of the terminal.
     status += chr(8)*(len(status)+1)
-    print status,
+    print(status,end="",file=log)
 f.close()
-print " [done]  "
+print(" [done]  ",file=log)
+if erase_older_nightly_slicer:
+    print("Removing previous nightly packages (from ",output_folder,")",sep="",file=log)
+    list_previous_nightly = [p for p in os.listdir(output_folder) if p != "SlicerCurrentNightly" ]
+    for p in list_previous_nightly:
+        old_nightly = os.path.join(output_folder, p)
+        print("Removing:",old_nightly,file=log)
+        try:
+            shutil.rmtree(old_nightly)
+        except OSError:
+            print("Failed to remove",old_nightly,file=log)
+            pass
 # Get Slicer directory
-print "Looking for Slicer nightly directory name (exploring the archive)"
+print("Looking for Slicer nightly directory name (exploring the archive)",file=log)
 process = subprocess.Popen(["tar", "-tf", output_file_name], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 process.wait()
 current_nightly = re.search('(.+?)'+os.sep+'Slicer', output).group(1)
-print "Directory found: "+current_nightly
+print("Directory found:",current_nightly,file=log)
 try:
     os.makedirs(output_folder)
 except OSError as exc:  # Python >2.5
@@ -83,26 +103,19 @@ except OSError as exc:  # Python >2.5
     else:
         raise
 # Extracting Slicer archive
-print "Extracting Slicer archive in "+output_folder
+print("Extracting Slicer archive in",output_folder,file=log)
 process = subprocess.Popen(["tar", "-xvzf", output_file_name, "-C", output_folder], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 process.wait()
-if erase_older_nightly_slicer:
-    print "Removing previous nightly packages (from "+output_folder+")"
-    list_previous_nightly = [p for p in os.listdir(output_folder) if p != current_nightly]
-    for p in list_previous_nightly:
-        old_nightly = os.path.join(output_folder, p)
-        print "Removing: "+old_nightly
-        try:
-            shutil.rmtree(old_nightly)
-        except OSError:
-            print "Failed to remove "+old_nightly
-            pass
 if os.name == "posix":
     current_slicer_exec = os.path.join(output_folder, current_nightly, "Slicer")
     symlink_name = os.path.join(output_folder, "SlicerCurrentNightly")
     try:
         os.remove(symlink_name)
+        print("Old link removed",file=log)
     except OSError:
         pass
     os.symlink(current_slicer_exec, symlink_name)
+    print("New link created",file=log)
+if log is not sys.stdout:
+  log.close()
